894936e20a7c548379337b8ab2bc5ccd86a172c4
barteks <bartlomiejszydlowski@gmail.com>
2018-07-16 09:46:20 -0400

    Add files via upload

---
+++ First_program.java
@@ -0,0 +1,45 @@
+
+public class First_program {
+
+	public static void main(String[] args) {
+/** Bart Szydlowski
+ ISM510 Introduction to Computer Programming for Business Applications
+ July 11 2018
+ */
+
+String types = "Data type specifies a set of values and their operations.\n" +
+		"JAVA has two categories of data: Primitive and Objective Data. \n" +
+		"There are 8 primitive data types\n";
+
+String variables = "A Java variable is a piece of memory that can contain a data value. \n" +
+		"A variable thus has a data type. Data types are covered in more detail in the text on\n" +
+		"Java data types. Variables are typically used to store information\n" +
+		"which your Java program needs to do its job.\n";
+String operators = "Operators in Java. Java provides many types of operators which can be \n" +
+		"used according to the need. They are classified based on the functionality \n" +
+		"they provide. Some of the types are:\n" +
+		"Arithmetic Operators\n" +
+		"Unary Operators\n" +
+		"Assignment Operator\n" +
+		"Relational Operators\n" +
+		"Logical Operators\n" +
+		"Ternary Operator\n" +
+		"Bitwise Operators\n" +
+		"Shift Operators\n" +
+		"instance of operator\n" +
+		"Precedence and Associativity\n" +
+		"Interesting Questions\n";
+String control = "Selection statements allow you to control the flow of program execution on the basis\n" +
+		"of the outcome of an expression or state of a variable known during runtime.\n" +
+		"Selection statements can be divided into the following categories:\n" +
+		"The if and if-else statements\n" +
+		"The if-else statements\n" +
+		"The if-else-if statements\n" +
+		"The switch statements";
+System.out.println(types);
+System.out.println(variables);
+System.out.println(operators);
+System.out.println(control);
+	}
+
+}
