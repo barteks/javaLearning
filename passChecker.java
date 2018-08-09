ed2ec9f778ca9294692cbc21a1d9d88ec7baddd1
barteks <bartlomiejszydlowski@gmail.com>
2018-08-08 22:26:58 -0400

    week5

--- passChecker.java
+++ passChecker.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -5,10 +6,14 @@ import javax.swing.JTextPane;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
 
 public class passChecker {
 
 	private JFrame frame;
+	private JTextField login;
+	private JTextField password;
+	private JTextField checked;
 
 	/**
 	 * Launch the application.
@@ -55,21 +60,44 @@ public class passChecker {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JTextPane login = new JTextPane();
-		login.setBounds(34, 75, 279, 40);
-		frame.getContentPane().add(login);
-		
 		JButton btnNewButton = new JButton("New button");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				String log = login.getText();
+				String pass = password.getText();
+			for(int i=0; i<users.length; i++) {
+				for (int j=0; j<users.length; j++) {
+					if ((pass == users[0][j]) && (log == users[1][j])) {
+					checked.setText("Your password is valid!");
+					checked.setBackground(Color.green);
+				}
+				else {
+					checked.setText("Your password is not valid");
+					checked.setBackground(Color.red);
+				}
+						
+			}
+						}
 			}
 		});
+					
+							
 		btnNewButton.setBounds(383, 259, 141, 35);
 		frame.getContentPane().add(btnNewButton);
 		
-		JTextPane password = new JTextPane();
-		password.setBounds(34, 160, 279, 40);
+		login = new JTextField();
+		login.setBounds(67, 58, 186, 32);
+		frame.getContentPane().add(login);
+		login.setColumns(10);
+		
+		password = new JTextField();
+		password.setBounds(67, 131, 186, 32);
 		frame.getContentPane().add(password);
+		password.setColumns(10);
+		
+		checked = new JTextField();
+		checked.setBounds(67, 260, 186, 32);
+		frame.getContentPane().add(checked);
+		checked.setColumns(10);
 	}
 }
